9009ad30095fa5cec7982f529ee58b4e8916af05
Darren <darrenawf@gmail.com>
2024-09-16 17:48:21 -0400

    cant scroll when popup is open

--- protfolio-script.js
+++ protfolio-script.js
@@ -1,9 +1,11 @@
 function project1(){
     document.getElementById("popUpBox").style.display = "block";
     document.getElementById("popUpBackground").style.display = "block";
+    document.body.style.overflowY = "hidden"; 
 }
 
 function popUpClose(){
     document.getElementById("popUpBox").style.display = "none";
     document.getElementById("popUpBackground").style.display = "none";
+    document.body.style.overflowY = "scroll"; 
 }
