765fb790d7b3973d1fd3b054c7458b880cb96fef
Darren <darrenawf@gmail.com>
2024-09-16 17:15:50 -0400

    background and pop up and javascript

---
+++ protfolio-script.js
@@ -0,0 +1,9 @@
+function project1(){
+    document.getElementById("popUpBox").style.display = "block";
+    document.getElementById("popUpBackground").style.display = "block";
+}
+
+function popUpClose(){
+    document.getElementById("popUpBox").style.display = "none";
+    document.getElementById("popUpBackground").style.display = "none";
+}
