f094a248e2b54ce54b8d0412ee870f1e2e1cc118
Darren <darrenawf@gmail.com>
2024-09-19 16:47:41 -0400

    gallery image scrolling added

--- protfolio-script.js
+++ protfolio-script.js
@@ -1,47 +1,41 @@
+//Define Arrays for each tab
+imageArr = [["images/monkeypedia/page1.png", "images/monkeypedia/page2.png", "images/monkeypedia/page3.png"], [], ["images/conathon/page1.png", "images/conathon/page2.png", "images/conathon/page3.png", "images/conathon/page4.png", "images/conathon/page5.png"], []];
+titleArr = ["Monkeypedia", "eCommerce", "Conathon's Casino", "Java Games"];
+// Image Index for Gallery
+let currentImageIndex = 0;
+
 function tab(num) { //Changes content to selected tab
+    //Define Arrays for each tab
+    linkArr = [["https://github.com/darrenawf/Monkeypedia-BETA"], [""], ["https://github.com/darrenawf/ConathonsCasino"], ["https://github.com/darrenawf/Dino-Game", "https://github.com/darrenawf/Flap-Burd"]];
+    //Set colour for closed tab
+    document.getElementById("tabOne").style.backgroundColor = "rgb(200, 205, 215)";
+    document.getElementById("tabTwo").style.backgroundColor = "rgb(200, 205, 215)";
+    document.getElementById("tabThree").style.backgroundColor = "rgb(200, 205, 215)";
+    document.getElementById("tabFour").style.backgroundColor = "rgb(200, 205, 215)";
+    //Set colour for open tab
     switch (num) {
         case 1:
-            //Tab
             document.getElementById("tabOne").style.backgroundColor = "rgb(210, 215, 225)"; //Display open tab
-            document.getElementById("tabTwo").style.backgroundColor = "rgb(200, 205, 215)";
-            document.getElementById("tabThree").style.backgroundColor = "rgb(200, 205, 215)";
-            document.getElementById("tabFour").style.backgroundColor = "rgb(200, 205, 215)";
-            //Content
-            document.querySelector(".popUpTitle").textContent = "Monkeypedia";
-            document.querySelector(".popUpTitleContainer a").href = "https://github.com/darrenawf/Monkeypedia-BETA";
             break;
         case 2:
-            //Tab
-            document.getElementById("tabOne").style.backgroundColor = "rgb(200, 205, 215)";
             document.getElementById("tabTwo").style.backgroundColor = "rgb(210, 215, 225)"; //Display open tab
-            document.getElementById("tabThree").style.backgroundColor = "rgb(200, 205, 215)";
-            document.getElementById("tabFour").style.backgroundColor = "rgb(200, 205, 215)";
-            //Content
-            document.querySelector(".popUpTitle").textContent = "eCommerce";
-            document.querySelector(".popUpTitleContainer a").href = "https://github.com/darrenawf/Monkeypedia-BETA";
             break;
         case 3:
-            //Tab background colours
-            document.getElementById("tabOne").style.backgroundColor = "rgb(200, 205, 215)";
-            document.getElementById("tabTwo").style.backgroundColor = "rgb(200, 205, 215)";
             document.getElementById("tabThree").style.backgroundColor = "rgb(210, 215, 225)"; //Display open tab
-            document.getElementById("tabFour").style.backgroundColor = "rgb(200, 205, 215)";
-            //Content
-            document.querySelector(".popUpTitle").textContent = "Conathon's Casino";
-            document.querySelector(".popUpTitleContainer a").href = "https://github.com/darrenawf/ConathonsCasino";
             break;
         case 4:
-            //Tab
-            document.getElementById("tabOne").style.backgroundColor = "rgb(200, 205, 215)";
-            document.getElementById("tabTwo").style.backgroundColor = "rgb(200, 205, 215)";
-            document.getElementById("tabThree").style.backgroundColor = "rgb(200, 205, 215)";
             document.getElementById("tabFour").style.backgroundColor = "rgb(210, 215, 225)"; //Display open tab
-            //Content
-            document.querySelector(".popUpTitle").textContent = "Java Games";
-            document.querySelector(".popUpTitleContainer a").href = "https://github.com/darrenawf/Dino-Game";
-            //document.querySelector(".popUpTitleContainer a").href = "https://github.com/darrenawf/Flap-Burd";
             break;
     }
+    //Title
+    document.querySelector(".popUpTitle").textContent = titleArr[num - 1];
+    //Link
+    document.querySelector(".popUpTitleContainer a").href = linkArr[num - 1][0];
+    //Images
+    document.querySelector(".popUpImage").src = imageArr[num - 1][1];
+    document.querySelector(".galleryImage.one").src = imageArr[num - 1][0];
+    document.querySelector(".galleryImage.two").src = imageArr[num - 1][1];
+    document.querySelector(".galleryImage.three").src = imageArr[num - 1][2];
 }
 
 function popUpOpen(num) { //Opens pop up window
@@ -57,10 +51,70 @@ function popUpClose() { //Closes pop up window
     document.body.style.overflowY = "scroll";
 }
 
+// Scroll image gallery to the left
 function galleryLeft() {
-    //SCROLL LEFT
+    let num;
+    title = document.querySelector(".popUpTitle").textContent;
+
+    // Determine the tab number based on the title
+    switch (title) {
+        case titleArr[0]:
+            num = 1;
+            break;
+        case titleArr[1]:
+            num = 2;
+            break;
+        case titleArr[2]:
+            num = 3;
+            break;
+        case titleArr[3]:
+            num = 4;
+            break;
+    }
+
+    // Get the total number of images for the selected tab
+    galleryLength = imageArr[num - 1].length;
+
+    // Decrement the current image index, wrapping around if necessary
+    // Set to left (+1)
+    currentImageIndex = (currentImageIndex + 1 + galleryLength) % galleryLength;
+
+    // Update the gallery images
+    document.querySelector(".galleryImage.one").src = imageArr[num - 1][currentImageIndex % galleryLength];
+    document.querySelector(".galleryImage.two").src = imageArr[num - 1][(currentImageIndex + 1) % galleryLength];
+    document.querySelector(".galleryImage.three").src = imageArr[num - 1][(currentImageIndex + 2) % galleryLength];
 }
 
+// Scroll image gallery to the left
 function galleryRight() {
-    //SCROLL RIGHT
+    let num;
+    title = document.querySelector(".popUpTitle").textContent;
+
+    // Determine the tab number based on the title
+    switch (title) {
+        case titleArr[0]:
+            num = 1;
+            break;
+        case titleArr[1]:
+            num = 2;
+            break;
+        case titleArr[2]:
+            num = 3;
+            break;
+        case titleArr[3]:
+            num = 4;
+            break;
+    }
+
+    // Get the total number of images for the selected tab
+    galleryLength = imageArr[num - 1].length;
+
+    // Decrement the current image index, wrapping around if necessary
+    //Set to Right (-1)
+    currentImageIndex = (currentImageIndex - 1 + galleryLength) % galleryLength; 
+
+    // Update the gallery images
+    document.querySelector(".galleryImage.one").src = imageArr[num - 1][currentImageIndex % galleryLength];
+    document.querySelector(".galleryImage.two").src = imageArr[num - 1][(currentImageIndex + 1) % galleryLength];
+    document.querySelector(".galleryImage.three").src = imageArr[num - 1][(currentImageIndex + 2) % galleryLength];
 }
